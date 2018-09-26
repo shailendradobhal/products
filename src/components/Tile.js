05de40ab7930649e173f6f974dbb62de5f371ac3
Shailendra Dobhal <sdobhal@sapient.com>
2018-09-26 15:41:28 +0530

    products

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Tile = ({product}) => {
   return (
     <div className="card w-25 m-3">
-      <img className="card-img-top" src={product.image} alt="Image" />
+      <img className="card-img-top" src={product.image} alt="Product Image" />
       <div className="card-body">
         <h5 className="card-title">{product.name}</h5>
         <p className="card-text">{product.description}</p>
       </div>
+      <div class="card-footer">Quantity: {product.quantity}</div>
     </div>
   );
 };
--- src/data/index.js
+++ src/data/index.js
@@ -4,69 +4,79 @@ export const products = [
 		type: 'Books',
 		description: 'A Tale of Two Cities by Charles Dickens is in part a historical novel, which sets it apart from Dickens\'s other work.',
 		name: 'A Tale of two cities',
-		image: './images/1.jpg'
+		image: './images/1.jpg',
+		quantity: 12
 	},
 	{
 		id: '3de432',
 		type: 'Books',
 		description: 'Emily Brontë, sister of Charlotte and Anne Brontë was born on July 30, 1818, in Yorkshire. Wuthering Heights, published in 1847.',
 		name: 'Wuthering Heights',
-		image: './images/2.jpg'
+		image: './images/2.jpg',
+		quantity: 4
 	},
 	{
 		id: '3de433',
 		type: 'Books',
 		description: 'Charles John Huffam Dickens; 7 February 1812 – 9 June 1870) was an English writer and social critic.',
 		name: 'Great Expectations',
-		image: './images/3.jpg'
+		image: './images/3.jpg',
+		quantity: 10
 	},
 	{
 		id: '3de434',
 		type: 'Books',
 		description: 'An American novelist and short story writer, whose works are the paradigmatic writings of the Jazz Age.',
 		name: 'The Great Gatsby',
-		image: './images/4.jpg'
+		image: './images/4.jpg',
+		quantity: 20
 	},
 	{
 		id: '3de435',
 		type: 'Books',
 		description: 'The short story has evolved through the ages from myths, legends, fairy tales, fables, parables.',
 		name: '50 World’s Greatest Short Stories',
-		image: './images/5.jpg'
+		image: './images/5.jpg',
+		quantity: 22
 	},
 	{
 		id: '3de436',
 		type: 'Books',
 		description: 'Jules Gabriel Verne was born in February 1828. He developed a passion for travelling and adventure at an early age.',
 		name: 'Around the World in Eighty Days',
-		image: './images/6.jpg'
+		image: './images/6.jpg',
+		quantity: 32
 	},
 	{
 		id: '3de437',
 		type: 'Books',
 		description: 'Think of the high seas and of a buccaneer ship; of a wild seaman with a sea chest full of gold; of Long John Silver.',
 		name: 'Treasure Island',
-		image: './images/7.jpg'
+		image: './images/7.jpg',
+		quantity: 90
 	},
 	{
 		id: '3de438',
 		type: 'Books',
 		description: 'A compelling science fiction, the Time Machine is a first-hand account of a Time Traveler\'s journey into the future.',
 		name: 'The Time Machine',
-		image: './images/8.jpg'
+		image: './images/8.jpg',
+		quantity: 111
 	},
 	{
 		id: '3de439',
 		type: 'Books',
 		description: 'On a summer night, at the end of the nineteenth century, a “meteor” lands on Horsell Common in London.',
 		name: 'The War of the Worlds',
-		image: './images/9.jpg'
+		image: './images/9.jpg',
+		quantity: 23
 	},
 	{
 		id: '3de440',
 		type: 'Books',
 		description: 'The colourless skein of life and our duty is to unravel it and isolate it and expose every inch of it.',
 		name: 'The Complete Novels of Sherlock Holmes',
-		image: './images/10.jpg'
+		image: './images/10.jpg',
+		quantity: 78
 	}
 ];
